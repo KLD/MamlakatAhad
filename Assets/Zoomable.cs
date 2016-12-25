3c08e93bfc2dc767d260e393c6614e0c69ba73cb
KLD0093 <KLD0093@hotmail.com>
2016-12-25 12:11:32 -0500

    Added Zoomability to world map.

---
+++ Assets/Zoomable.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zoomable : MonoBehaviour
+{
+    public float MinimumScale;
+    public float MaximumScale; 
+
+    void Update()
+    {
+        float power = Input.GetAxis("Mouse ScrollWheel"); 
+        if (power != 0f)
+        {
+
+            if (transform.localScale.x + power < MinimumScale)
+            {
+                transform.localScale = new Vector3(MinimumScale, MinimumScale);
+            }
+            else if (transform.localScale.x + power > MaximumScale)
+            {
+                transform.localScale = new Vector3(MaximumScale, MaximumScale);
+            }
+            else
+            {
+                transform.localScale = new Vector3(transform.localScale.x + power, transform.localScale.y + power);
+            }
+        }
+    }
+
+
+}
